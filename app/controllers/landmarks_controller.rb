4258a0dada98e9f6609723f37f12766ad5551d73
Arel English <arelenglish@gmail.com>
2014-03-05 17:13:18 -0500

    removed stuff

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,10 +1,3 @@
 class LandmarksController < ApplicationController
-  get '/landmarks' do
-    erb :'landmarks/index'
-  end
-
-  get '/landmark/:id' do
-    @landmark = Landmark.find(params[:id])
-    erb :"landmarks/show"
-  end
+  
 end
