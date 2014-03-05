b2f95e60a21c024f7074ce054ad443c0c6672b22
Arel English <arelenglish@gmail.com>
2014-03-05 14:51:00 -0500

    started adding solution

---
+++ db/migrate/01_create_landmarks.rb
@@ -0,0 +1,10 @@
+class CreateLandmarks < ActiveRecord::Migration
+def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+
+      t.timestamps
+    end
+  end
+end
