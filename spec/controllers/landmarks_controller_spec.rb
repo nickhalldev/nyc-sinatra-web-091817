02a8b70b7c982ac78a78b662616bb784f64c8eca
Arel English <arelenglish@gmail.com>
2014-03-05 17:11:48 -0500

    changed some things

--- spec/controllers/landmarks_controller_spec.rb
+++ spec/controllers/landmarks_controller_spec.rb
@@ -5,6 +5,10 @@ describe LandmarksController do
     Landmark.create(name: 'BQE', year_completed: '1961')
   end
 
+  after do
+    Landmark.destroy_all
+  end
+
   it "allows you to list all landmarks" do
     get '/landmarks'
     expect(last_response.status).to eq(200)
