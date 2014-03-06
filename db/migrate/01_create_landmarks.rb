6a320b34a557be0fc2ce92f62e45c15c6bf5e137
Arel English <arelenglish@gmail.com>
2014-03-06 10:27:39 -0500

    added migration

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,10 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise "Write CreateLandmarks migration here"
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+
+      t.timestamps
+    end
+  end
 end
