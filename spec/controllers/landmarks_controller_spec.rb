2a8c3b9b91f30572e5c182e6b5ee0d5aa08e29ac
ruchiramani <ruchiramani@gmail.com>
2016-03-24 10:47:18 -0400

    Solves most of #24

--- spec/controllers/landmarks_controller_spec.rb
+++ spec/controllers/landmarks_controller_spec.rb
@@ -74,4 +74,16 @@ describe LandmarksController do
     expect(@landmark.year_completed.to_s).to eq("9999")
 
   end
+
+  it "creates checkboxes for all the landmarks and titles created on the Figures new page" do 
+    Landmark.create(name: 'BQE', year_completed: 1961)
+      visit "/figures/new"
+      expect(page).to have_css("input[type=\"checkbox\"]")
+      expect(page).to have_content('BQE')
+     Title.create(:name => "Mayor")
+      visit "/figures/new"
+      expect(page).to have_css("input[type=\"checkbox\"]")
+      expect(page).to have_content('Mayor')
+      
+  end
 end
